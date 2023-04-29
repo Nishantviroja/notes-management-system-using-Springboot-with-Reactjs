cb5a6788e4718fa351e65d12b9c7d2018b39747f
Nishant Viroja <81224226+Nishantviroja@users.noreply.github.com>
2023-04-30 01:23:31 +0530

    /notes api ready for show all notes data

--- springboot-backend/src/main/java/com/notesmanagementsystem/springbootbackend/controller/NoteController.java
+++ springboot-backend/src/main/java/com/notesmanagementsystem/springbootbackend/controller/NoteController.java
@@ -4,9 +4,12 @@ import com.notesmanagementsystem.springbootbackend.model.Note;
 import com.notesmanagementsystem.springbootbackend.repository.NoteRepository;
 import org.aspectj.weaver.ast.Not;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class NoteController {
@@ -17,5 +20,10 @@ public class NoteController {
     @PostMapping("/note")
     Note newNote(@RequestBody Note newNote){
         return noteRepository.save(newNote);
+    }
+
+    @GetMapping("/notes")
+    List<Note> getAllNotes() {
+        return noteRepository.findAll();
     }
 }
