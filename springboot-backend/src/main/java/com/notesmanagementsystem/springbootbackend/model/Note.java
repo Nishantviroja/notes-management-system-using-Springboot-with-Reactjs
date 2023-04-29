a5e834d809c60f9a553aa0be68d72a85eae7e641
Nishant Viroja <81224226+Nishantviroja@users.noreply.github.com>
2023-04-29 15:17:47 +0530

    database create successfully.

--- springboot-backend/src/main/java/com/notesmanagementsystem/springbootbackend/model/Note.java
+++ springboot-backend/src/main/java/com/notesmanagementsystem/springbootbackend/model/Note.java
@@ -1,9 +1,8 @@
 package com.notesmanagementsystem.springbootbackend.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class Note {
@@ -46,5 +45,4 @@ public class Note {
     public void setTag(String tag) {
         this.tag = tag;
     }
-    
 }
